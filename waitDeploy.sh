b264395ab9fc22e5a176d2a8021c463d303e3410
Luis Alexandre Deschamps Brandão <techmago@ymail.com>
2018-01-16 17:57:51 -0200

    Add security checks to waitDeploy

--- waitDeploy.sh
+++ waitDeploy.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 # Desenvolvido por Luis Alexandre Deschamps Brandão
+# brandao_luis@yahoo.com
 # Script de deploy com aguardo de finalização
+
+# Testa se foi passado o número correto de paramentos
+if [ ! ${#} -eq 1 ] ;	then
+  echo "FATAL: I did not receive the input json"
+  exit 1
+fi
+
+if [ ! -s "${1}" ]; then
+  echo "FATAL: json file doesn't exists or is empty"
+  exit 1
+fi
 
 # Extrai o ID do deploy.
 deploymentID=$(jq '.deploymentId' ${1} | sed 's/\"//g' )
 deploymentRegion=${2}
 
 failSafe=0
-while [ $failSafe -lt 60 ] ; do
+while [ $failSafe -lt 90 ] ; do
   failSafe=$(($failSafe+1))
 
   deploymentJson=$(aws deploy get-deployment --deployment-id ${deploymentID} --region ${deploymentRegion})
