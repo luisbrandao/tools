eb5363bbc8cef3abd19a5fd128bfb6737b1a2fff
Luis Alexandre Deschamps Brandão <luis.deschamps.ext@sascar.com.br>
2018-11-22 13:30:48 -0200

    Complete version of wlst weblogic deploy script

--- weblogicDeploy.py
+++ weblogicDeploy.py
@@ -7,9 +7,9 @@ from java.io import File
 def usage():
     print "Usage:"
     print "deployWLST.py [-t timeout] [-i interval]"
-def connectConsole(weblogicPassword,weblogicURL):
-    connect('weblogic', weblogicPassword, weblogicURL)
-    print '-'*100+"\n" + "Connected to weblogic: " + weblogicURL + "\n"+'-'*100
+def connectConsole(weblogicEndpoint, weblogicUser, weblogicPassword):
+    connect(weblogicUser, weblogicPassword, weblogicEndpoint)
+    print '-'*100+"\n" + "Connected to weblogic: " + weblogicEndpoint + "\n"+'-'*100
 def removeAplication(appName):
     deployed_application_names = [];
     app_deployments = cmo.getAppDeployments()
@@ -24,15 +24,15 @@ def removeAplication(appName):
         print '-'*100+"\n" + "Aplication stop and undeploy sucess" + "\n"+'-'*100
     else:
         print '-'*100+"\n" + appName + ": Doesnt exist" + "\n"+'-'*100
-def deployAplication(appName):
+def deployAplication(appName, clusterName):
     print '-'*100+"\n" + "Starting deploy of " + appName + "\n"+'-'*100
-    deploy(appName, artifact, targets='cl_integracao', upload='true')
+    deploy(appName, artifact, targets=clusterName, upload='true')
     startApplication(appName)
     print '-'*100+"\n" + "Deploy sucess" + "\n"+'-'*100
 
 #====== Main program ===============================
 try:
-    opts, args = getopt.getopt( sys.argv[0:], "u:p:n:a:b", ["weblogicURL","weblogicPassword","appName","artifact","batata"] )
+    opts, args = getopt.getopt( sys.argv[1:], "e:u:p:n:a:c:", ["weblogicEndpoint","weblogicUser","weblogicPassword","appName","artifact","clusterName"] )
 except getopt.GetoptError, err:
     print str(err)
     usage()
@@ -40,24 +40,19 @@ except getopt.GetoptError, err:
 
 #===== Handling get options  ===============
 for opt, arg in opts:
-    if opt == "-u":
-        weblogicURL = arg
+    if opt == "-e":
+        weblogicEndpoint = arg
+    elif opt == "-u":
+        weblogicUser = arg
     elif opt == "-p":
         weblogicPassword = arg
     elif opt == "-n":
         appName = arg
     elif opt == "-a":
         artifact = arg
-    elif opt == "-b":
-        batata = arg
+    elif opt == "-c":
+        clusterName = arg
 
-
-print "weblogicURL: " + weblogicURL
-print "weblogicPassword: " + weblogicPassword
-print "appName: " + appName
-print "artifact: " + artifact
-print "batata: " + batata
-
-#connectConsole(weblogicPassword,weblogicURL)
-#removeAplication(appName)
-#deployAplication(appName)
+connectConsole(weblogicEndpoint, weblogicUser, weblogicPassword)
+removeAplication(appName)
+deployAplication(appName,clusterName)
