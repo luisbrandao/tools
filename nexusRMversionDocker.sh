eec368bcb93ebf1c94704288186027ed411ef5ed
Luis Alexandre Deschamps Brandão <luis.brandao@agrotis.com>
2019-12-19 13:40:44 -0300

    suport multiple versions

--- nexusRMversionDocker.sh
+++ nexusRMversionDocker.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
-group=${1}
-version=${2}
+# Example: bash nexusRMversionDocker.sh acc-backend black* blue-37 purple-11*
 
+# Variables --------------------------------------------------------------------
 credential='admin:password'
 url_base='http://nexus3.example.local/service/rest/v1'
 api_url='http://nexus3.example.local/repository/local-registry/v2'
+i=0
+# ------------------------------------------------------------------------------
 
-for URL in $(curl -X GET "${url_base}/search?format=docker&name=${group}&version=${version}" -s | jq -r '.items[].assets[].downloadUrl') ; do
-	echo ${URL}
+# Check minimum arguments
+if [ "$#" -le 2 ]; then
+    echo 'I need at least 2 arguments!'
+    echo 'example: bash nexusRMversionDocker.sh acc-backend black* blue-37 purple-11*'
+    exit 1
+fi
+
+# parse the arguments
+group=${1} ; shift
+while [[ $# -gt 0 ]] ; do
+  versions[${i}]=${1}
+  i=$((i+1))
+  shift
+done
+
+# Prompt user
+for version in ${versions[*]} ; do
+  for URL in $(curl -X GET "${url_base}/search?format=docker&name=${group}&version=${version}" -s | jq -r '.items[].assets[].downloadUrl') ; do
+  	echo ${URL}
+  done
 done
 
 read -p "Are you sure? " -n 1 -r
@@ -16,10 +36,12 @@ if [[ $REPLY =~ ^[Yy]$ ]] ; then
   echo ==========================
   echo ======= Apagando =========
   echo ==========================
-  for HASH in $(curl -X GET "${url_base}/search?format=docker&name=${group}&version=${version}" -s | jq -r '.items[].assets[].checksum.sha256') ; do
-    URL="${api_url}/${group}/manifests/sha256:${HASH}"
-    echo ${URL}
-  	curl -X DELETE --user "${credential}" ${URL}
+  for version in ${versions[*]} ; do
+    for HASH in $(curl -X GET "${url_base}/search?format=docker&name=${group}&version=${version}" -s | jq -r '.items[].assets[].checksum.sha256') ; do
+      URL="${api_url}/${group}/manifests/sha256:${HASH}"
+      echo ${URL}
+    	curl -X DELETE --user "${credential}" ${URL}
+    done
   done
 else
   echo ==========================
--- nexusRMversionMVN.sh
+++ nexusRMversionMVN.sh
@@ -1,24 +1,45 @@
 #!/bin/bash
-group=${1}
-version=${2}
+# Example: bash bash nexusRMversionMVN.sh br.com.example.acc green* purple.111 yellow.*
 
+# Variables --------------------------------------------------------------------
 credential='admin:password'
 url_base='http://nexus3.example.local/service/rest/v1'
+i=0
+# ------------------------------------------------------------------------------
 
-for URL in $(curl -X GET "${url_base}/search?group=${group}&version=${version}" -s | jq -r '.items[].assets[].downloadUrl') ; do
-	echo ${URL}
+# Check minimum arguments
+if [ "$#" -le 2 ]; then
+    echo 'I need at least 2 arguments!'
+    echo 'bash nexusRMversionMVN.sh br.com.example.acc green* purple.111 yellow.*'
+    exit 1
+fi
+
+# parse the arguments
+group=${1} ; shift
+while [[ $# -gt 0 ]] ; do
+  versions[${i}]=${1}
+  i=$((i+1))
+  shift
 done
 
-#read -p "Are you sure? " -n 1 -r
-#echo    # (optional) move to a new line
-REPLY=y
+# Prompt the user
+for version in ${versions[*]} ; do
+  for URL in $(curl -X GET "${url_base}/search?group=${group}&version=${version}" -s | jq -r '.items[].assets[].downloadUrl') ; do
+  	echo ${URL}
+  done
+done
+
+read -p "Are you sure? " -n 1 -r
+echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]] ; then
   echo ==========================
   echo ======= Apagando =========
   echo ==========================
-  for URL in $(curl -X GET "${url_base}/search?group=${group}&version=${version}" -s | jq -r '.items[].assets[].downloadUrl') ; do
-    echo ${URL}
-  	curl -X DELETE --user "${credential}" ${URL}
+  for version in ${versions[*]} ; do
+    for URL in $(curl -X GET "${url_base}/search?group=${group}&version=${version}" -s | jq -r '.items[].assets[].downloadUrl') ; do
+      echo ${URL}
+    	curl -X DELETE --user "${credential}" ${URL}
+    done
   done
 else
   echo ==========================
