d0f3bb36c119190dca61339dc089315958c79af2
Luis Alexandre <techmago@ymail.com>
2022-04-18 14:33:08 -0300

    Nexus mvn-continuation

--- nexusRMversionMVN.sh
+++ nexusRMversionMVN.sh
@@ -23,12 +23,24 @@ while [[ $# -gt 0 ]] ; do
   shift
 done
 
+rm -f list.txt
 # Prompt the user
 for version in ${versions[*]} ; do
-  for URL in $(curl -X GET "${url_base}/search?group=${group}&version=${version}" -s | jq -r '.items[].assets[].downloadUrl') ; do
-  	echo ${URL}
+  echo "For version: ${version}"
+  # First request
+  curl -X GET "${url_base}/search?group=${group}&version=${version}" -s > request.json
+  jq -r '.items[].assets[].downloadUrl' request.json >> list.txt
+  tkn=$(jq -r '.continuationToken' request.json) ; if [ ${tkn} = "null" ] ; then unset tkn ; fi
+  # While there is a continuationToken
+  while [ ! -z "${tkn}" ] ; do
+    curl -X GET "${url_base}/search?group=${group}&version=${version}&continuationToken=${tkn}" -s > request.json
+    jq -r '.items[].assets[].downloadUrl' request.json >> list.txt
+    tkn=$(jq -r '.continuationToken' request.json) ; if [ ${tkn} = "null" ] ; then unset tkn ; fi
   done
 done
+
+# Processes result
+cat list.txt
 
 read -p "Are you sure? " -n 1 -r
 echo    # (optional) move to a new line
@@ -36,14 +48,13 @@ if [[ $REPLY =~ ^[Yy]$ ]] ; then
   echo ==========================
   echo ======= Apagando =========
   echo ==========================
-  for version in ${versions[*]} ; do
-    for URL in $(curl -X GET "${url_base}/search?group=${group}&version=${version}" -s | jq -r '.items[].assets[].downloadUrl') ; do
-      echo ${URL}
-    	curl -X DELETE --user "${credential}" ${URL}
-    done
+  for URL in $(cat list.txt) ; do
+    echo ${URL}
+  	curl -X DELETE --user "${credential}" ${URL}
   done
 else
   echo ==========================
   echo ======== abort ===========
   echo ==========================
 fi
+rm -f list.txt
