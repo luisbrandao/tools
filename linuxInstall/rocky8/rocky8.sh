5966afef51db9c09dcad1c66d3c9f050796aad88
Luis Alexandre Deschamps Brandao <techmago@ymail.com>
2022-07-04 13:06:05 -0300

    Update rocky8 install script

--- linuxInstall/rocky8/rocky8.sh
+++ linuxInstall/rocky8/rocky8.sh
@@ -76,7 +76,7 @@ wget --no-check-certificate https://raw.githubusercontent.com/luisbrandao/tools/
 mv techmago.sh /etc/profile.d/
 
 # Remove programas inuteis ==================================================================================================
-dnf remove -y abrt* postfix crash empathy hypervkvpdy qemu-guest-agent spice-vdagent open-vm-tools gnome-boxes
+dnf remove -y abrt* postfix crash empathy hypervkvpdy qemu-guest-agent spice-vdagent open-vm-tools gnome-boxes orca
 rpm -e --nodeps kernel-rx-headers
 
 # Instala pacotes ===========================================================================================================
@@ -114,10 +114,9 @@ pacotes="${pacotes} htop iotop iftop pydf bmon pydf inxi nload"
 pacotes="${pacotes} ntpdate fortune-mod gnome-disk-utility terminator bash-completion"
 pacotes="${pacotes} net-tools mlocate psmisc hddtemp lm_sensors glances"
 pacotes="${pacotes} ntfs-3g ntfsprogs fuse-exfat exfat-utils"
-pacotes="${pacotes} wine winetricks flatpak"
+pacotes="${pacotes} wine winetricks flatpak stress"
 pacotes="${pacotes} xprop libwnck3 xwininfo xdotool"
 pacotes="${pacotes} java-latest-openjdk java-11-openjdk"
-
 
 if [ "${devel}" = yes ]; then
 	pacotes="${pacotes} unison sshfs byobu nfs-utils gparted"
@@ -142,28 +141,32 @@ dnf -y --nogpg --skip-broken --best --allowerasing install ${pacotes} ; recheck_
 # pacotes="${pacotes} texmaker texlive-scheme-small texlive-collection-langportuguese texlive-supertabular texlive-tocloft texlive-hyphenat texlive-moderncv"
 
 # Desenvolvimento
-pacotes=""
-pacotes="${pacotes} java-11-openjdk java-11-openjdk-devel maven"
-pacotes="${pacotes} java-1.8.0-openjdk java-1.8.0-openjdk-devel"
-pacotes="${pacotes} gtk2-immodules mono-core mono-devel"
-pacotes="${pacotes} acpid elfutils-libelf-devel  kernel-tools-libs  kernel-tools kernel-modules-extra kernel-modules kernel-devel linux-firmware"
-pacotes="${pacotes} mesa-dri-drivers.i686 mesa-dri-drivers.x86_64 mesa-filesystem.i686 mesa-filesystem.x86_64 mesa-libEGL.i686 mesa-libEGL.x86_64"
-pacotes="${pacotes} mesa-libEGL-devel.i686 mesa-libEGL-devel.x86_64 mesa-libGL.i686 mesa-libGL.x86_64 mesa-libGL-devel.i686 mesa-libGL-devel.x86_64"
-pacotes="${pacotes} mesa-libGLU.i686 mesa-libGLU.x86_64 mesa-libGLU-devel.i686"
-pacotes="${pacotes} mesa-libGLU-devel.x86_64 mesa-libGLw-devel.i686 mesa-libGLw-devel.x86_64 mesa-libOSMesa.i686 mesa-libOSMesa.x86_64"
-pacotes="${pacotes} mesa-libOSMesa-devel.i686 mesa-libgbm.i686 mesa-libgbm.x86_64 mesa-libgbm-devel.i686"
-pacotes="${pacotes} mesa-libgbm-devel.x86_64 mesa-libglapi.i686 mesa-libglapi.x86_64 mesa-libGLw.x86_64 mesa-libGLw.i686"
-pacotes="${pacotes} libmodplug-devel libv4l-devel libva-devel openal-soft-devel opus-devel schroedinger-devel"
-pacotes="${pacotes} soxr-devel texinfo x265-devel ilbc-devel SDL-devel aalib-devel bzip2-devel alsa-lib-devel enca-devel faad2-devel"
-pacotes="${pacotes} ffmpeg-devel fribidi-devel giflib-devel gsm-devel gtk2-devel ladspa-devel lame-devel libXinerama-devel libXScrnSaver-devel"
-pacotes="${pacotes} libXv-devel libXvMC-devel libass-devel libbs2b-devel libcaca-devel libcdio-paranoia-devel libdca-devel"
-pacotes="${pacotes} libdv-devel libmpeg2-devel libmpg123-devel librtmp-devel libtheora-devel libvdpau-devel libvorbis-devel"
-pacotes="${pacotes} lirc-devel lzo-devel pulseaudio-libs-devel speex-devel x264-devel xvidcore-devel yasm dbus-glib-devel"
-pacotes="${pacotes} gtk3-devel libcurl-devel libgpod-devel libnotify-devel nautilus-devel"
-pacotes="${pacotes} qt5-linguist qt5-qtbase-devel qt5-qtscript-devel qt5-qttools-devel qt5-qtwebkit-devel qtsingleapplication-qt5-devel"
-dnf -y --nogpg --skip-broken --best --allowerasing install ${pacotes} ; recheck_retry "${pacotes}"
+if [ "${devel}" = yes ]; then
+  pacotes=""
+  pacotes="${pacotes} java-11-openjdk java-11-openjdk-devel maven"
+  pacotes="${pacotes} java-1.8.0-openjdk java-1.8.0-openjdk-devel"
+  pacotes="${pacotes} gtk2-immodules mono-core mono-devel"
+  pacotes="${pacotes} acpid elfutils-libelf-devel  kernel-tools-libs  kernel-tools kernel-modules-extra kernel-modules kernel-devel linux-firmware"
+  pacotes="${pacotes} mesa-dri-drivers.i686 mesa-dri-drivers.x86_64 mesa-filesystem.i686 mesa-filesystem.x86_64 mesa-libEGL.i686 mesa-libEGL.x86_64"
+  pacotes="${pacotes} mesa-libEGL-devel.i686 mesa-libEGL-devel.x86_64 mesa-libGL.i686 mesa-libGL.x86_64 mesa-libGL-devel.i686 mesa-libGL-devel.x86_64"
+  pacotes="${pacotes} mesa-libGLU.i686 mesa-libGLU.x86_64 mesa-libGLU-devel.i686"
+  pacotes="${pacotes} mesa-libGLU-devel.x86_64 mesa-libGLw-devel.i686 mesa-libGLw-devel.x86_64 mesa-libOSMesa.i686 mesa-libOSMesa.x86_64"
+  pacotes="${pacotes} mesa-libOSMesa-devel.i686 mesa-libgbm.i686 mesa-libgbm.x86_64 mesa-libgbm-devel.i686"
+  pacotes="${pacotes} mesa-libgbm-devel.x86_64 mesa-libglapi.i686 mesa-libglapi.x86_64 mesa-libGLw.x86_64 mesa-libGLw.i686"
+  pacotes="${pacotes} libmodplug-devel libv4l-devel libva-devel openal-soft-devel opus-devel schroedinger-devel"
+  pacotes="${pacotes} soxr-devel texinfo x265-devel ilbc-devel SDL-devel aalib-devel bzip2-devel alsa-lib-devel enca-devel faad2-devel"
+  pacotes="${pacotes} ffmpeg-devel fribidi-devel giflib-devel gsm-devel gtk2-devel ladspa-devel lame-devel libXinerama-devel libXScrnSaver-devel"
+  pacotes="${pacotes} libXv-devel libXvMC-devel libass-devel libbs2b-devel libcaca-devel libcdio-paranoia-devel libdca-devel"
+  pacotes="${pacotes} libdv-devel libmpeg2-devel libmpg123-devel librtmp-devel libtheora-devel libvdpau-devel libvorbis-devel"
+  pacotes="${pacotes} lirc-devel lzo-devel pulseaudio-libs-devel speex-devel x264-devel xvidcore-devel yasm dbus-glib-devel"
+  pacotes="${pacotes} gtk3-devel libcurl-devel libgpod-devel libnotify-devel nautilus-devel"
+  pacotes="${pacotes} qt5-linguist qt5-qtbase-devel qt5-qtscript-devel qt5-qttools-devel qt5-qtwebkit-devel qtsingleapplication-qt5-devel"
 
-# Hack para deletar arquivos
+  dnf -y --nogpg --skip-broken --best --allowerasing install ${pacotes} ; recheck_retry "${pacotes}"
+fi
+
+
+# Hack para deletar arquivos no atom ========================================================================================
 echo '#!/usr/bin/env bash
 # GVFS updated and dropped gvfs-trash to gio, but Atom didnt update.
 # https://github.com/atom/tree-view/issues/1237
